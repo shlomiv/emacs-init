d67df6f3f87e38c28ce8b4a530dd3977f6a8462c
Shlomi Vaknin <shlomi.vaknin@intel.com>
2016-09-08 00:56:30 -0700

    add files

---
+++ init.el
@@ -0,0 +1,12 @@
+(setq warning-minimum-level :emergency)
+
+(add-to-list 'load-path "~/org-mode/org-mode/lisp")
+(add-to-list 'load-path "~/org-mode/org-mode/contrib/lisp" )
+
+
+;; init.el for this setup. Must use Emacs 24
+(org-babel-load-file
+ (expand-file-name "emacs-init.org"
+		  "~/shlomi-emacs"))
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
